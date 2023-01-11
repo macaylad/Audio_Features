16182e96b6490dbea032a45ec3c2488658d473b6
macaylad <macayla.donegan@mindstronghealth.com>
2023-01-11 15:40:20 -0500

    anxiety index added

---
+++ audio_measure_functions.py
@@ -0,0 +1,57 @@
+import process_audio
+import audio_features
+
+
+def get_loneliness_index(file_name_m4a, verbose=bool):
+    file_name, audio_data, samplerate = process_audio.convert_extract_audio_data(file_name_m4a)
+    text, words = process_audio.get_text_from_speech(file_name, audio_data)
+
+    sent_score = audio_features.get_sentiment(text)
+    filler_score = audio_features.get_fillers(words)
+    pitch_score = audio_features.get_pitch_variability(file_name)
+    formant2_score = audio_features.get_2formant_variabilty(file_name)
+    pause_score = audio_features.get_av_pause_len(audio_data)
+
+    loneliness_index = sent_score + filler_score + pitch_score + formant2_score + pause_score
+
+    if verbose:
+        return loneliness_index, sent_score, filler_score, pitch_score, formant2_score, pause_score, text
+    else:
+        return loneliness_index
+
+
+def get_depression_index(file_name_m4a, verbose=bool):
+    file_name, audio_data, samplerate = process_audio.convert_extract_audio_data(file_name_m4a)
+    text, words = process_audio.get_text_from_speech(file_name, audio_data)
+
+    sent_score = audio_features.get_sentiment(text)
+    filler_score = audio_features.get_fillers(words)
+    pitch_score = audio_features.get_pitch_variability(file_name)
+    pause_score = audio_features.get_av_pause_len(audio_data)
+    mfcc2_score = audio_features.get_mfcc2(audio_data, samplerate)
+    rate_of_speech = audio_features.get_speech_rate(words, audio_data, samplerate)
+
+    depression_index = sent_score + filler_score + pitch_score + mfcc2_score + pause_score + rate_of_speech
+
+    if verbose:
+        return depression_index, sent_score, filler_score, pitch_score, mfcc2_score, pause_score, text
+    else:
+        return depression_index
+
+
+def get_anxiety_index(file_name_m4a, verbose=bool):
+    file_name, audio_data, samplerate = process_audio.convert_extract_audio_data(file_name_m4a)
+    text, words = process_audio.get_text_from_speech(file_name, audio_data)
+
+    f0_score = audio_features.get_f0(audio_data, samplerate) ## same as f0?
+    shimmer_score = audio_features.get_shimmer(file_name)
+    pause_score = audio_features.get_number_of_pauses(audio_data,samplerate)
+    hnr_score = audio_features.get_hnr(file_name)
+
+    anxiety_index = f0_score + shimmer_score + pause_score + hnr_score
+
+    if verbose:
+        return anxiety_index, f0_score, shimmer_score, pause_score, hnr_score, text
+    else:
+        return anxiety_index
+
